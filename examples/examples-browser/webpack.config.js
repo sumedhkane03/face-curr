5c7ebd231ae29dd4d740d9996cbd50984741dbf2
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-06-12 14:35:53 -0400

    fiing-build

--- examples/examples-browser/webpack.config.js
+++ examples/examples-browser/webpack.config.js
@@ -1,29 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './server.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './views/webcamFaceLandmarkDetection.html'
-    })
-  ],
-  optimization: {
-    emitOnErrors: false
-  }
+  target: 'node',
 };
